1aee39f814dd02b1a7f8b5e0aefe2402d860b7c1
RegMeasures <Richard.measures@niwa.co.nz>
2018-11-27 17:14:44 +1300

    Starting to read in spatial inputs Reading in shoreline position and position of river inflow to the lagoon and starting to process them into model coordinate system.

--- hapuamod.py
+++ hapuamod.py
@@ -1,9 +1,10 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
-#import numpy as np
+import numpy as np
 #import calendar
 from configobj import ConfigObj
 import os
+import shapefile
 
 def loadModel(ModelConfigFile):
     
@@ -21,4 +22,39 @@ def loadModel(ModelConfigFile):
     SeaLevelFile = os.path.join(ConfigFilePath, 
                                 config['Boundary conditions']['SeaLevel'])
     SeaLevelTs = pd.read_csv(SeaLevelFile, index_col=0)
+    
+    # Read the initial shoreline position
+    ShoreShpFile = os.path.join(ConfigFilePath, 
+                                config['Spatial inputs']['Shoreline'])
+    ShoreShp = shapefile.Reader(ShoreShpFile)
+    # check it is a polyline and there is only one line
+    assert ShoreShp.shapeType==3, 'Shoreline shapefile must be a polyline.'
+    assert len(ShoreShp.shapes())==1, 'multiple polylines in Shoreline shapefile. There should only be 1.'
+    # extract coordinates
+    IniShoreCoords = np.asarray(ShoreShp.shape(0).points[:])
+    
+    # Read the river inflow location
+    RiverShpFile = os.path.join(ConfigFilePath, 
+                                config['Spatial inputs']['RiverLocation'])
+    RiverShp = shapefile.Reader(RiverShpFile)
+    # check it is a single point
+    assert RiverShp.shapeType==1, 'Shoreline shapefile must be a point.'
+    assert len(RiverShp.shapes())==1, 'multiple points in RiverLocation shapefile. There should only be 1.'
+    InflowCoord = np.asarray(RiverShp.shape(0).points[:])
+    
+    # Make a straight reference baseline
+    Baseline = np.polyfit(IniShoreCoords[:,0], IniShoreCoords[:,1], 1)
+    
+    # correct shoreline orientation: land (and river inflow) should be to left of line
+        
+    # Interpolate points at intervals along shoreline
+    
+    
+    
+    # Produce a map showing the spatial inputs
+    plt.plot(IniShoreCoords[:,0], IniShoreCoords[:,1],'bx')
+    plt.plot(IniShoreCoords[:,0], IniShoreCoords[:,0] * Baseline[0] + Baseline[1],'k:')
+    plt.plot(InflowCoord[0,0], InflowCoord[0,1],'ro')
+    plt.axis('equal')
+    
     return (FlowTs, WaveTs, SeaLevelTs)
