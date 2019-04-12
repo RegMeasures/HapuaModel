1-line model evolution Implement time-stepping coast evolution for 1-line shoreline model Also fixed bugs in treatment of wave angles and calculation of LST

TestHapuaMod.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 import hapuamod as hm
 
@@ -20,39 +21,63 @@ except NameError:
 ModelConfigFile = 'inputs\HurunuiModel.cnf'
 Config = hm.load.readConfig(ModelConfigFile)
 (FlowTs, WaveTs, SeaLevelTs, Origin, BaseShoreNormDir, ShoreX, ShoreY, LagoonY,
- LagoonElev, RiverElev, OutletX, OutletY, OutletDx, OutletElev, OutletWidth, 
+ LagoonElev, RiverElev, OutletX, OutletY, OutletElev, OutletWidth, 
  Dx, Dt, SimTime, PhysicalPars) = hm.load.loadModel(Config)
 
-hm.visualise.mapView(ShoreX, ShoreY, LagoonY, Origin, BaseShoreNormDir)
-hm.visualise.modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY)
+#plt.figure()
+#hm.visualise.mapView(ShoreX, ShoreY, LagoonY, Origin, BaseShoreNormDir)    
+
+#plt.figure(figsize=(12,5))
+#hm.visualise.modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY)
 
 #%% Test longshore transport routine
-EAngle_h = WaveTs.EAngle_h[0]
+EDir_h = WaveTs.EDir_h[0]
 WavePower = WaveTs.WavePower[0]
 WavePeriod = WaveTs.WavePeriod[0]
 Wlen_h = WaveTs.Wlen_h[0]
 
+plt.figure()
+plt.subplot(3, 1, 1)
+plt.plot(ShoreX, ShoreY)
+
 LST = hm.coast.longShoreTransport(ShoreY, Dx, WavePower, WavePeriod, Wlen_h, 
-                                EAngle_h, PhysicalPars)
+                                EDir_h, PhysicalPars)
+plt.subplot(3, 1, 2)
+plt.plot((ShoreX[0:-1]+ShoreX[1:])/2, LST)
 
 Dy = hm.coast.shoreChange(LST, Dx, Dt, PhysicalPars)
+plt.subplot(3, 1, 3)
+plt.plot(ShoreX, Dy)
 
 #%% Test river routines
 # Join river and outlet through lagoon
 (ChanDx, ChanElev, ChanWidth, ChanArea) = \
-    hm.riv.assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx, 
-                           OutletElev, OutletWidth, OutletX, 
+    hm.riv.assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, 
+                           OutletX, OutletY, OutletElev, OutletWidth, 
                            PhysicalPars['RiverWidth'], Dx)
 
 # Steady state hydraulics
-ChanDep = hm.riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
-                             PhysicalPars['Roughness'], 
-                             FlowTs[0], SeaLevelTs[0])
+RivFlow = FlowTs[0]
+SeaLevel = SeaLevelTs[0]
+(ChanDep, ChanVel) = hm.riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
+                                        PhysicalPars['Roughness'], 
+                                        RivFlow, SeaLevel)
 
-hm.visualise.longSection(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx, 
-                         OutletElev, OutletWidth, OutletX, 
+hm.visualise.longSection(RiverElev, ShoreX, LagoonY, LagoonElev, 
+                         OutletElev, OutletWidth, OutletX, OutletY,
                          PhysicalPars['RiverWidth'], Dx)
-
 
 ChanDist = np.insert(np.cumsum(ChanDx),0,0)
 plt.plot(ChanDist, ChanDep+ChanElev)
+
+#%% Test core timestepping
+Time = SimTime[0]
+CurrentDay = Time.day
+plt.figure(figsize=(12,5))
+hm.visualise.modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY)
+while Time <= SimTime[1]:
+    hm.core.timestep(Time, Dx, Dt, WaveTs, SeaLevelTs, FlowTs, PhysicalPars,
+                     ShoreX, ShoreY, LagoonY, LagoonElev, RiverElev, 
+                     OutletX, OutletY, OutletElev, OutletWidth)
+    Time += Dt
+hm.visualise.modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY)

hapuamod/coast.py
@@ -3,7 +3,7 @@
 # import standard packages
 import numpy as np
 
-def longShoreTransport(ShoreY, Dx, WavePower, WavePeriod, Wlen_h, EAngle_h, PhysicalPars):
+def longShoreTransport(ShoreY, Dx, WavePower, WavePeriod, Wlen_h, EDir_h, PhysicalPars):
     """ Calculates longshore transport rate for each shore segment
     
     Calculation performed for whole model for a single timestep.
@@ -21,7 +21,7 @@ def longShoreTransport(ShoreY, Dx, WavePower, WavePeriod, Wlen_h, EAngle_h, Phys
     
     # Calculate offshore wave angle relative to each shoreline segment
     LocalRelShoreDir = np.arctan((ShoreY[0:-1] - ShoreY[1:])/Dx)
-    LocalEAngle_h = EAngle_h - LocalRelShoreDir
+    LocalEDir_h = EDir_h - LocalRelShoreDir
     
     # Calculate breaking wave depth
     BreakerDepth = (PhysicalPars['BreakerCoef'] * WavePower)**0.2
@@ -31,9 +31,9 @@ def longShoreTransport(ShoreY, Dx, WavePower, WavePeriod, Wlen_h, EAngle_h, Phys
     C_h = ((PhysicalPars['Gravity'] * WavePeriod / (2.0 * np.pi)) * 
            np.tanh(2.0 * np.pi * PhysicalPars['WaveDataDepth'] / Wlen_h))
     # sin(alpha_b)/sin(alpha_h) = C_b/C_h
-    BreakerAngle = np.arcsin(np.sin(LocalEAngle_h) * (C_b / C_h))
+    BreakerAngle = np.arcsin(np.sin(LocalEDir_h) * (C_b / C_h))
     
-    LST = PhysicalPars['K2coef'] * WavePower * np.cos(LocalEAngle_h) * np.sin(BreakerAngle)
+    LST = - PhysicalPars['K2coef'] * WavePower * np.cos(LocalEDir_h) * np.sin(BreakerAngle)
     
     return LST
 
@@ -57,7 +57,7 @@ def shoreChange(LST, Dx, Dt, PhysicalPars):
     # TODO: handle shoreline boundary conditions
     
     Dy = np.zeros([LST.size + 1])
-    Dy[1:-1] = (LST[0:-1] - LST[1:]) * Dt / (PhysicalPars['ClosureDepth'] * Dx)
+    Dy[1:-1] = (LST[0:-1] - LST[1:]) * Dt.seconds / (PhysicalPars['ClosureDepth'] * Dx)
     
     return Dy
     

hapuamod/load.py
@@ -54,8 +54,10 @@ def loadModel(Config):
     variables. Prior to running loadModel the onfig file should first be parsed
     using the readConfig function.
     
-    (FlowTs, WaveTs, SeaLevelTs, Origin, BaseShoreNormDir, 
-     ShoreX, ShoreY, Dx) = loadModel(Config)
+    (FlowTs, WaveTs, SeaLevelTs, Origin, BaseShoreNormDir, ShoreX, 
+     ShoreY, LagoonY, LagoonElev, RiverElev, OutletX, OutletY, 
+     OutletElev, OutletWidth, Dx, Dt, SimTime, 
+     PhysicalPars) = loadModel(Config)
     
     Parameters:
         Config (Dict): Model config file read into a dict variable by the 
@@ -77,7 +79,8 @@ def loadModel(Config):
                       radians as part of the pre-processing (radians)
         SeaLevelTs (DataFrame): Sea level timeseries as single column DataFrame
             with datetime index. Column name is SeaLevel (m)
-        Origin
+        Origin (np.ndarray(float64)): Real world X and Y coordinates of the
+            origin of the model coordinate system (m)
         BaseShoreNormDir (float): Direction of offshore pointing line at 90 
             degrees to overall average coast direction. Computed based on a 
             straightline fitted thruogh the initial condition shoreline 
@@ -87,24 +90,42 @@ def loadModel(Config):
         LagoonY (np.ndarray(float64)): position of seaward and landward sides 
             of the lagoon in model coordinate system at transects with 
             x-coordinates given by ShoreX (m)
+        LagoonElev (np.ndarray(float64)): 
+        RiverElev (np.ndarray(float64)): 
+        OutletX, OutletY (np.ndarray(float64)): coordinates of discretised 
+            outlet channel nodes in model coordinate system (m)
+        OutletElev (np.ndarray(float64)): 
+        OutletWidth (np.ndarray(float64)): 
         Dx (float): shoreline discretisation interval (m)
-        Dt (float): timestep (s)
+        Dt (pd.Timedelta): timestep
         SimTime (list): Two element list containing the simulation start
             and end times (datetime)
         PhysicalPars (dict): Physical parameters including:
-            RhoSed (float)
-            RhoSea (float)
-            RhoRiv (float)
-            Gravity (float)
-            Kcoef (float)
-            VoidRatio (float)
-            GammaRatio (float)
-            WaveDataDepth (float)
+            RhoSed (float): Sediment density (kg/m3)
+            RhoSea (float): Seawater density (kg/m3)
+            RhoRiv (float): Riverwater density (kg/m3)
+            Gravity (float): Gravity (m/s2)
+            Kcoef (float): K coefficient for longshore transport 
+                (non-dimensional)
+            VoidRatio (float): Sediment void ratio (0-1)
+            GammaRatio (float): Ratio of water depth at breakpoint to breaking 
+                wave height (non-dimensional)
+            WaveDataDepth (float): Depth contour of input wave data (m)
+            ClosureDepth (float): Closure depth for 1-line shoreline model (m)
+            RiverSlope (float): Initial condition slope for river upstream of 
+                hapua (m/m)
+            GrainSize (float): Sediment size for fluvial sediment transport 
+                calculation (assumed uniform) (m) 
+            UpstreamLength (float): Length of river upstream of hapua to 
+                include in model (m)
+            RiverWidth (float): width of river upstream of hapua (assumed 
+                       uniform) (m)
+            Roughness (float): Manning's 'n' for river hydraulics (m^(1/3)/s) 
             K2coef (float): Calculated from other inputs for use in calculation
                 of longshore transport rate. K2 = K / (RhoSed - RhoSea) * g * (1 - VoidRatio))
             BreakerCoef (float): Calculated from other inputs for use in 
                 calculation of depth of breaking waves. 
-                BreakerCoef = 8 / (RhoSea * Gravity^1.5 * GammaRatio^2)
+                BreakerCoef = 8 / (RhoSea * Gravity^1.5 * GammaRatio^2)                        
     """
     
     #%% Spatial inputs
@@ -179,9 +200,9 @@ def loadModel(Config):
                          index_col=0, parse_dates=True,
                          infer_datetime_format=True)
     # Convert wave directions into radians in model coordinate system
-    WaveTs.EAngle_h = np.deg2rad(WaveTs.EAngle_h) - (BaseShoreNormDir)
+    WaveTs.EDir_h = np.deg2rad(WaveTs.EDir_h) - (BaseShoreNormDir)
     # Make sure all wave angles are in the range -pi to +pi
-    WaveTs.EAngle_h = np.mod(WaveTs.EAngle_h + np.pi, 2.0 * np.pi) - np.pi 
+    WaveTs.EDir_h = np.mod(WaveTs.EDir_h + np.pi, 2.0 * np.pi) - np.pi 
     
     logging.info('Reading sea level timeseries from "%s"' % 
                  Config['BoundaryConditions']['SeaLevel'])
@@ -197,7 +218,7 @@ def loadModel(Config):
                'LagoonBed': float(Config['InitialConditions']['LagoonBed'])}
     
     #%% Time inputs
-    Dt = float(Config['Time']['Timestep'])
+    Dt = pd.Timedelta(seconds=float(Config['Time']['Timestep']))
     SimTime = [pd.to_datetime(Config['Time']['StartTime']),
                pd.to_datetime(Config['Time']['EndTime'])]
     
@@ -295,14 +316,10 @@ def loadModel(Config):
     (OutletX, OutletY) = geom.adjustLineDx(OutletX, OutletY, Dx)
     
     # Create outlet channel variables
-    OutletDx = np.sqrt((OutletX[1:]-OutletX[0:-1])**2 + 
-                       (OutletY[1:]-OutletY[0:-1])**2)
     OutletElev = np.linspace(IniCond['LagoonBed'], IniCond['OutletBed'],
                              OutletX.size)
     OutletWidth = np.tile(IniCond['OutletWidth'], OutletX.size)
-    
-  
-    
+      
     # Produce a map showing the spatial inputs
 #    (ShoreXreal, ShoreYreal) = geom.mod2real(ShoreX, ShoreY, Origin, BaseShoreNormDir)
 #    plt.plot(IniShoreCoords[:,0], IniShoreCoords[:,1], 'bx')
@@ -311,11 +328,7 @@ def loadModel(Config):
 #    plt.plot(InflowCoord[0], InflowCoord[1],'ro')
 #    plt.plot(Origin[0], Origin[1], 'go')
 #    plt.axis('equal')
-        
-
-    
-    
     
     return (FlowTs, WaveTs, SeaLevelTs, Origin, BaseShoreNormDir, ShoreX, 
-            ShoreY, LagoonY, LagoonElev, RiverElev, OutletX, OutletY, OutletDx, 
+            ShoreY, LagoonY, LagoonElev, RiverElev, OutletX, OutletY, 
             OutletElev, OutletWidth, Dx, Dt, SimTime, PhysicalPars)

hapuamod/riv.py
@@ -5,8 +5,8 @@
 import numpy as np
 import logging
 
-def assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx, 
-                    OutletElev, OutletWidth, OutletX, RiverWidth, Dx):
+def assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletX, OutletY,
+                    OutletElev, OutletWidth, RiverWidth, Dx):
     """ Combine river, lagoon and outlet into single channel for hyd-calcs
     
     (ChanDx, ChanElev, ChanWidth, ChanArea) = \
@@ -14,7 +14,8 @@ def assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx,
                             OutletElev, OutletWidth, OutletX, RiverWidth, Dx)
     """
     LagoonWidth = LagoonY[:,1] - LagoonY[:,0]
-    
+    OutletDx = np.sqrt((OutletX[1:]-OutletX[0:-1])**2 + 
+                       (OutletY[1:]-OutletY[0:-1])**2)
     # TODO: Account for potentially dry parts of the lagoon when 
     #       calculating ChanArea
     
@@ -47,9 +48,14 @@ def assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx,
     return (ChanDx, ChanElev, ChanWidth, ChanArea)
 
 def solveSteady(ChanDx, ChanElev, ChanWidth, Roughness, Qin, DsWL):
+    """ Solve steady state river hydraulics
     
+    (ChanDep, ChanVel) = solveSteady(ChanDx, ChanElev, ChanWidth, 
+                                     Roughness, Qin, DsWL)
+    """
     Grav = 9.81
     Tol = 0.005
+    MaxIter = 10
     
     # Find critical depth
     # Fr = Vel/sqrt(Grav*Dep) = 1 i.e. Vel^2 = Grav*Dep
@@ -88,7 +94,7 @@ def solveSteady(ChanDx, ChanElev, ChanWidth, Roughness, Qin, DsWL):
             ChanDep[XS] -= DepErr / Gradient
             DepErr = ChanDep[XS] + Acoef*ChanDep[XS]**(-2) - Bcoef*ChanDep[XS]**(-10/3) + Cconst
             CheckCount += 1
-            assert CheckCount < 10, 'Maximum iterations exceeded solving steady state water level'
+            assert CheckCount < MaxIter, 'Maximum iterations exceeded solving steady state water level'
         
         # Check for supercritical
         if ChanDep[XS] < CritDep[XS]:
@@ -98,4 +104,4 @@ def solveSteady(ChanDx, ChanElev, ChanWidth, Roughness, Qin, DsWL):
         Energy[XS] = ChanElev[XS] + ChanDep[XS] + ChanVel[XS]**2 / (2*Grav)
         S_f[XS] = ChanVel[XS]**2 * Roughness**2 / ChanDep[XS]**(4/3)
     
-    return ChanDep      
+    return ChanDep, ChanVel

hapuamod/visualise.py
@@ -11,8 +11,6 @@ import hapuamod.riv as riv
 def mapView(ShoreX, ShoreY, LagoonY, Origin, ShoreNormalDir):
     """ Map the current model state in real world coordinates
     """
-    # Create new figure
-    plt.figure()
     
     # Plot the shoreline
     (ShoreXreal, ShoreYreal) = geom.mod2real(ShoreX, ShoreY, Origin, ShoreNormalDir)
@@ -38,8 +36,6 @@ def mapView(ShoreX, ShoreY, LagoonY, Origin, ShoreNormalDir):
 def modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY):
     """ Map the current model state in model coordinates
     """
-    # Create new figure
-    plt.figure(figsize=(12,5))
     
     # Plot shoreline
     plt.plot(ShoreX, ShoreY, 'k-')
@@ -56,8 +52,8 @@ def modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY):
     
     plt.axis('equal')
     
-def longSection(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx, 
-                OutletElev, OutletWidth, OutletX, RiverWidth, Dx):
+def longSection(RiverElev, ShoreX, LagoonY, LagoonElev, 
+                OutletElev, OutletWidth, OutletX, OutletY, RiverWidth, Dx):
     """ View a long section of the river to the lagoon outlet
     """
     # Create new figure
@@ -65,8 +61,8 @@ def longSection(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx,
     
     # Assemble the complete channel
     (ChanDx, ChanElev, ChanWidth, ChanArea) = \
-        riv.assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletDx, 
-                            OutletElev, OutletWidth, OutletX, RiverWidth, Dx)
+        riv.assembleChannel(RiverElev, ShoreX, LagoonY, LagoonElev, OutletX, 
+                            OutletY, OutletElev, OutletWidth, RiverWidth, Dx)
     
     # Plot the river bed level
     ChanDist = np.insert(np.cumsum(ChanDx),0,0)

preprocessing/WaveDataFromSwanOutput.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 WaveModelOutputFile = 'C:\\Users\measuresrj\OneDrive - NIWA\Hapua\Hurunui\SWAN\output\TS_hurunui.txt'
-ShoreNormalDir = 128 # [degrees]
+ShoreNormalDir = 128 # [degrees] used for back refracting to deep water
 
 #%% Read model wave data
 ColNames = ['ModelTime', 'Year', 'Month', 'Day', 'Hour', 'Min', 
@@ -21,7 +21,8 @@ OutputData = WaveData.loc[:,['WavePeriod','Wlen_h']]
 #%% Calculate net wave power magnitude and direction
 OutputData['WavePower'] = 1025 * 9.81 * np.sqrt(np.power(WaveData['XWForce'],2) + 
                                                 np.power(WaveData['YWForce'],2))
-OutputData['EAngle_h'] = np.rad2deg(np.arctan2(WaveData['XWForce'], WaveData['YWForce']))
+OutputData['EDir_h'] = np.rad2deg(np.arctan2(-WaveData['XWForce'], -WaveData['YWForce']))
+OutputData['EDir_h'] = OutputData['EDir_h'] % 360
 
 #%% Calculate offshore significant wave height
 