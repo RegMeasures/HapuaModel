RegMeasures/HapuaModel

commit 4abcdcc2a707f5750fb77b0d9ac538f5bc9fc93e

Adjusting outlet channel length as it migrates trimming/extending channel and re-positioning nodes as necessary as it migrates. Partially addresses #7

hapuamod/core.py
@@ -47,9 +47,6 @@ def run(ModelConfigFile):
                             OutletX, OutletY, OutletElev, OutletWidth, 
                             PhysicalPars['RiverWidth'], NumericalPars['Dx'])
     
-    BarrierHeight = np.ones(ChanElev.size) * 3
-    # TODO remove above line by dealing with bank height correctly!
-    
     (ChanDep, ChanVel) = riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
                                          PhysicalPars['Roughness'], 
                                          RivFlow, SeaLevel)

hapuamod/geom.py
@@ -114,11 +114,16 @@ def trimSegment(LineX, LineY, TrimLineX, TrimLineY):
     
     return (NewLineX, NewLineY)
     
-def adjustLineDx(LineX, LineY, MaxDx):
+def adjustLineDx(LineX, LineY, MaxDx, *args):
     """ Move points on line to maintain Dx within target range
     
     (NewLineX, NewLineY) = adjustLineDx(LineX, LineY, MaxDx)
+        or
+    (NewLineX, NewLineY, NewLineProperties)
+        = adjustLineDx(LineX, LineY, MaxDx, LineProperties)
     """
+    LineProperties = list(args)
+    
     MinDx = 0.4 * MaxDx
     DefaultDx = 0.7 * MaxDx
     
@@ -136,38 +141,37 @@ def adjustLineDx(LineX, LineY, MaxDx):
     while np.any(SegLen < MinDx):
         Shortest = np.which(SegLen == np.amin(SegLen))[0]
         if Shortest == 0:
-            RemNode = 1
+            RemoveNode = 1
         elif Shortest == SegLen.size-1:
-            RemNode = Shortest
+            RemoveNode = Shortest
         elif SegLen[Shortest-1] <= SegLen[Shortest+1]:
-            RemNode = Shortest
+            RemoveNode = Shortest
         else:
-            RemNode = Shortest + 1
+            RemoveNode = Shortest + 1
         
-        SegLen = np.concatenate([SegLen[:RemNode-1],
-                                 SegLen[RemNode-1]+SegLen[RemNode],
-                                 SegLen[RemNode:]])
+        SegLen[RemoveNode-1] = SegLen[RemoveNode-1] + SegLen[RemoveNode]
+        SegLen = np.delete(SegLen, RemoveNode)
+        LineX = np.delete(LineX, RemoveNode)
+        LineY = np.delete(LineY, RemoveNode)
+        for Property in LineProperties:
+            Property = np.delete(Property, RemoveNode)
     
     # Split segments which are too long
     TooLong = np.where(SegLen > MaxDx)[0]
-    for Seg in np.nditer(TooLong):
-        SplitInto = int(np.ceil(SegLen[Seg]/DefaultDx))
-        SegLen = np.concatenate((SegLen[:Seg],
-                                 np.tile(SegLen[Seg] / SplitInto, SplitInto),
-                                 SegLen[Seg+1:]))
-    
-    # Re-interpolate line properties
-    NewCumSegLen = np.cumsum(SegLen)
-    NewLineX = np.concatenate([[LineX[0]], 
-                               np.interp(NewCumSegLen[0:-1], CumSegLen, LineX),
-                               [LineX[-1]]])
-    NewLineY = np.concatenate([[LineY[0]], 
-                               np.interp(NewCumSegLen[0:-1], CumSegLen, LineY),
-                               [LineY[-1]]])
-    
-    return (NewLineX, NewLineY)
-    
-    # TODO: Extend function to interpolate line properties (supplied as new optional input(S?))
+    for SegNo in np.nditer(TooLong):
+        SplitInto = int(np.ceil(SegLen[SegNo]/DefaultDx))
+        LineX = np.insert(LineX, SegNo, 
+                          LineX[SegNo-1] + (LineX[SegNo] - LineX[SegNo-1]) 
+                                           * (np.linspace(0,1,SplitInto,False)[1:]))
+        LineY = np.insert(LineY, SegNo, 
+                          LineY[SegNo-1] + (LineY[SegNo] - LineY[SegNo-1]) 
+                                           * (np.linspace(0,1,SplitInto,False)[1:]))
+        for Property in LineProperties:
+            Property = np.insert(Property, SegNo, 
+                                 Property[SegNo-1] + (Property[SegNo] - Property[SegNo-1]) 
+                                                     * (np.linspace(0,1,SplitInto,False)[1:]))
+
+    return tuple([LineX, LineY] + LineProperties)
 
 def shiftLineSideways(LineX, LineY, Shift):
     """ Apply lateral shift to a line by moving XY node coordinates

hapuamod/mor.py
@@ -118,5 +118,18 @@ def updateMorphology(LST, Bedload,
     # TODO use actual barrier height, split L and R bank calculations and account for differences in bank height and movement of channel centerline!
     OutletBankElev = 3.0
     OutletWidth += (OutletLbEro / ((OutletBankElev-OutletElev) * OutletDx2) 
-                    + OutletRbEro / ((OutletBankElev-OutletElev) * OutletDx2)) 
+                    + OutletRbEro / ((OutletBankElev-OutletElev) * OutletDx2))
     geom.shiftLineSideways(OutletX, OutletY, (OutletRbEro-OutletLbEro)/2)
+    
+    # trim outlet channel ends as necessary
+    (OutletX, OutletY) = geom.trimSegment(OutletX, OutletY, 
+                                          ShoreX, ShoreY)
+    (OutletX, OutletY) = geom.trimSegment(np.flipud(OutletX), np.flipud(OutletY), 
+                                          ShoreX, LagoonY[:,1])
+    OutletX = np.flipud(OutletX)
+    OutletY = np.flipud(OutletY)
+    assert OutletX.size == OutletElev.size, 'trimSegment has changed number of nodes in outlet channel without adjusting associated outlet channel properties'
+    
+    # adjust outlet channel segmentation as necessary
+    (OutletX, OutletY, OutletElev, OutletWidth) = \
+        geom.adjustLineDx(OutletX, OutletY, Dx, OutletElev, OutletWidth)

commit 5c2a2ec01d983856405dbf3ac8eeda8ca123bf2d

Allow updating (animating) plan view visualisation Also bug fix to geom.adjustLineDx (bug introduced in 4abcdcc).

hapuamod/core.py
@@ -62,6 +62,7 @@ def run(ModelConfigFile):
         LsLines = visualise.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel, 
                                         np.zeros(ChanElev.size))
         BdyFig = visualise.BdyCndFig(OutputTs)
+        ModelFig = visualise.modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY)
     
     #%% Main timestepping loop
     MorTime = TimePars['StartTime']
@@ -132,6 +133,8 @@ def run(ModelConfigFile):
                                             ChanWidth, ChanDep, ChanVel, 
                                             Bedload)
                 visualise.updateBdyCndFig(BdyFig, OutputTs)
+                visualise.updateModelView(ModelFig, ShoreX, ShoreY, LagoonY, 
+                                          OutletX, OutletY)
                 PlotTime += OutputOpts['PlotInt']
         
         # increment time

hapuamod/geom.py
@@ -157,18 +157,18 @@ def adjustLineDx(LineX, LineY, MaxDx, *args):
             Property = np.delete(Property, RemoveNode)
     
     # Split segments which are too long
-    TooLong = np.where(SegLen > MaxDx)[0]
-    for SegNo in np.nditer(TooLong):
+    TooLong = np.where(SegLen > MaxDx)[0].tolist()
+    for SegNo in TooLong:
         SplitInto = int(np.ceil(SegLen[SegNo]/DefaultDx))
-        LineX = np.insert(LineX, SegNo, 
-                          LineX[SegNo-1] + (LineX[SegNo] - LineX[SegNo-1]) 
+        LineX = np.insert(LineX, SegNo+1, 
+                          LineX[SegNo] + (LineX[SegNo+1] - LineX[SegNo]) 
                                            * (np.linspace(0,1,SplitInto,False)[1:]))
-        LineY = np.insert(LineY, SegNo, 
-                          LineY[SegNo-1] + (LineY[SegNo] - LineY[SegNo-1]) 
+        LineY = np.insert(LineY, SegNo+1, 
+                          LineY[SegNo] + (LineY[SegNo+1] - LineY[SegNo]) 
                                            * (np.linspace(0,1,SplitInto,False)[1:]))
         for Property in LineProperties:
-            Property = np.insert(Property, SegNo, 
-                                 Property[SegNo-1] + (Property[SegNo] - Property[SegNo-1]) 
+            Property = np.insert(Property, SegNo+1, 
+                                 Property[SegNo] + (Property[SegNo+1] - Property[SegNo]) 
                                                      * (np.linspace(0,1,SplitInto,False)[1:]))
 
     return tuple([LineX, LineY] + LineProperties)

hapuamod/visualise.py
@@ -6,7 +6,6 @@ import numpy as np
 
 # import local packages
 from hapuamod import geom
-from hapuamod import riv
 
 def mapView(ShoreX, ShoreY, LagoonY, Origin, ShoreNormalDir):
     """ Map the current model state in real world coordinates
@@ -35,37 +34,78 @@ def mapView(ShoreX, ShoreY, LagoonY, Origin, ShoreNormalDir):
 
 def modelView(ShoreX, ShoreY, LagoonY, OutletX, OutletY):
     """ Map the current model state in model coordinates
+    
+    Parameters:
+        ShoreX
+        ShoreY
+        LagoonY
+        OutletX
+        OutletY
+        
+    Returns:
+        ModelFig = (PlanFig, PlanAx, ShoreLine, LagoonLine, OutletLine)
     """
     
+    # Create a new figure
+    PlanFig, PlanAx = plt.subplots()
+    PlanAx.axis('equal')
+    PlanAx.set_xlabel('Alongshore distance [m]')
+    PlanAx.set_ylabel('Crossshore distance [m]')
+    
     # Plot shoreline
-    plt.plot(ShoreX, ShoreY, 'k-')
+    ShoreLine = PlanAx.plot(ShoreX, ShoreY, 'k-')
     
     # Plot lagoon (inc closing ends)
-    plt.plot(ShoreX, LagoonY[:,0], 'b-')
-    plt.plot(ShoreX, LagoonY[:,1], 'b-')
-    EndTransects = np.where(np.isnan(LagoonY[:,0])==False)[0][[0,-1]]
-    plt.plot([ShoreX[EndTransects[0]],ShoreX[EndTransects[0]]], LagoonY[EndTransects[0],:], 'b-')
-    plt.plot([ShoreX[EndTransects[1]],ShoreX[EndTransects[1]]], LagoonY[EndTransects[1],:], 'b-')
+    LagoonMask = np.isnan(LagoonY[:,1])==False
+    PlotLagoonX = np.concatenate(([ShoreX[np.where(LagoonMask)[0][0]]],
+                                  ShoreX[LagoonMask], 
+                                  np.flipud(ShoreX[LagoonMask])))
+    PlotLagoonY = np.concatenate(([LagoonY[np.where(LagoonMask)[0][0],0]],
+                                  LagoonY[:,1][LagoonMask], 
+                                  np.flipud(LagoonY[:,0][LagoonMask])))
+    LagoonLine = plt.plot(PlotLagoonX, PlotLagoonY, 'b-')
     
     # Plot Outlet channel
-    plt.plot(OutletX, OutletY, 'r-x')
+    OutletLine = plt.plot(OutletX, OutletY, 'r-x')
     
-    plt.axis('equal')
+    ModelFig = (PlanFig, ShoreLine, LagoonLine, OutletLine)
+    
+    return ModelFig
+
+def updateModelView(ModelFig, ShoreX, ShoreY, LagoonY, OutletX, OutletY):
+    
+    # Calculate required variables to plot
+    LagoonMask = np.isnan(LagoonY[:,1])==False
+    PlotLagoonX = np.concatenate(([ShoreX[np.where(LagoonMask)[0][0]]],
+                                  ShoreX[LagoonMask], 
+                                  np.flipud(ShoreX[LagoonMask])))
+    PlotLagoonY = np.concatenate(([LagoonY[np.where(LagoonMask)[0][0],0]],
+                                  LagoonY[:,1][LagoonMask], 
+                                  np.flipud(LagoonY[:,0][LagoonMask])))
+    
+    # Update the lines
+    ModelFig[1][0].set_data(ShoreX, ShoreY)
+    ModelFig[2][0].set_data(PlotLagoonX, PlotLagoonY)
+    ModelFig[3][0].set_data(OutletX, OutletY)
+    
     
 def longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel, Bedload):
-    """ View a long section of the river to the lagoon outlet
+    """ Create a long section of the river to the lagoon outlet
+    
+    LongSecFig = longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel, 
+                             Bedload)
     
     Parameters:
         ChanDx
         ChanElev
+        ChanWidth
         ChanDep
         ChanVel
+        Bedload
     
     Returns:
-        WaterLine
-        EnergyLine
-        VelLine
-        FrLine
+        LongSecFig = (RivFig, BedLine, WaterLine, EnergyLine, WidthLine, 
+                      VelLine, FrLine, QsLine)
     """
     g = 9.81
     Dist = np.insert(np.cumsum(ChanDx),0,0)