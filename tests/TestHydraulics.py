9ecbb9906a40aacbdd67baee0c82f92bcfde9fb3
RegMeasures <Richard.measures@niwa.co.nz>
2019-06-16 20:23:08 +1200

    Output hydraulics test to html with plots for easy viewing Issue #21

--- tests/TestHydraulics.py
+++ tests/TestHydraulics.py
@@ -3,6 +3,7 @@
 Tests for the 1D fluvial hydraulics model component
 """
 
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,21 +15,27 @@ import hapuamod.riv as riv
 import hapuamod.visualise as vis
 
 #%% Set up logging
+LogFile = 'HydraulicsTestLog.html'
+if os.path.isfile(LogFile):
+    os.remove(LogFile)
+
 RootLogger = logging.getLogger()
 RootLogger.setLevel(logging.INFO)
 
 ConsoleHandler = logging.StreamHandler()
 ConsoleHandler.setLevel(logging.INFO)
 
-FileHandler = logging.FileHandler('TestLog.txt')
+FileHandler = logging.FileHandler(LogFile)
 FileHandler.setLevel(logging.INFO)
+LogFormatter = logging.Formatter('<p> %(message)s </p>')
+FileHandler.setFormatter(LogFormatter)
 
 RootLogger.addHandler(ConsoleHandler)
 RootLogger.addHandler(FileHandler)
 
 #%% Test 1
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 1: steady flow through very simple channel with no offline storage')
+logging.info('<hr>')
+logging.info('<h1>Test 1: steady flow through very simple channel with no offline storage</h1>')
 
 # Setup
 ChanDx = np.full(50, 20.0)
@@ -53,6 +60,7 @@ NumericalPars = {'Beta':1.1,
                  'MaxIt':20,
                  'WarnTol':0.1}
 HydDt = pd.Timedelta(seconds=5)
+BetaSteady = 1.1
 
 Qin = 70
 Q_Ts = np.full(100, Qin)
@@ -61,11 +69,11 @@ DsWl_Ts = np.full(100, DsWL)
 
 # Steady solution
 (ChanDep, ChanVel) = riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
-                                     Roughness, NumericalPars['Beta'], Qin, DsWL)
+                                     Roughness, BetaSteady, Qin, DsWL)
 SteadyDep = ChanDep.copy()
 SteadyVel = ChanVel.copy()
 
-SteadyTime = timeit.timeit(stmt='riv.solveSteady(ChanDx, ChanElev, ChanWidth, Roughness, NumericalPars["Beta"], Qin, DsWL)', 
+SteadyTime = timeit.timeit(stmt='riv.solveSteady(ChanDx, ChanElev, ChanWidth, Roughness, BetaSteady, Qin, DsWL)', 
                            globals=globals(), number=20)
 logging.info('Steady solution took %f s' % SteadyTime)
 
@@ -78,14 +86,21 @@ try:
     # Reporting
     DepErr = ChanDep - SteadyDep
     VelErr = ChanVel - SteadyVel
-    # vis.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
+    LongSecFig = vis.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
+    LongSecFig[0].suptitle('Test 1: Steady flow')
+    LongSecFig[1].plot(Dist, ChanElev + SteadyDep, 'm--', label='Steady WL')
+    LongSecFig[4].plot(Dist, SteadyVel, 'm--', label='Steady vel')
+    LongSecFig[5].plot(Dist, abs(SteadyVel)/np.sqrt(9.81*SteadyDep), 'm--', label='Steady Fr')
+    LongSecFig[0].savefig('Test01_LongSection.png')
+    plt.close(LongSecFig[0])
+    logging.info('<img src="Test01_LongSection.png">')
     logging.info('Maximum difference between steady and unsteady solutions is: Depth %f m and velocity %f m/s' % (np.max(np.abs(DepErr)), np.max(np.abs(VelErr))))
 except:
     logging.exception('Unsteady solution failed during test 1')
 
 #%% Test2
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 2: unsteady flow reverting to steady')
+logging.info('<hr>')
+logging.info('<h1>Test 2: Unsteady flow reverting to steady</h1>')
 
 # Setup
 Q_Ts = np.concatenate([np.linspace(Qin, Qin*2, 50),
@@ -103,14 +118,20 @@ try:
     DepErr = ChanDep - SteadyDep
     VelErr = ChanVel - SteadyVel
     LongSecFig = vis.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
-    LongSecFig[0].suptitle('Test 2: unsteady flow reverting to steady')
+    LongSecFig[1].plot(Dist, ChanElev + SteadyDep, 'm--', label='Steady WL')
+    LongSecFig[4].plot(Dist, SteadyVel, 'm--', label='Steady vel')
+    LongSecFig[5].plot(Dist, (abs(SteadyVel)/np.sqrt(9.81*SteadyDep)), 'm--', label='Steady Fr')
+    LongSecFig[0].suptitle('Test 2: Unsteady flow reverting to steady')
+    LongSecFig[0].savefig('Test02_LongSection.png')
+    plt.close(LongSecFig[0])
+    logging.info('<img src="Test02_LongSection.png">')
     logging.info('Maximum difference between steady and unsteady solutions is: Depth %f m and velocity %f m/s' % (np.max(np.abs(DepErr)), np.max(np.abs(VelErr))))
 except:
     logging.exception('Unsteady solution failed during test 2')
     
 #%% Test 3
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 3: unsteady d/s boundary')
+logging.info('<hr>')
+logging.info('<h1>Test 3: unsteady d/s boundary</h1>')
 
 # Setup
 DsWl_Ts = np.concatenate([np.linspace(DsWL, DsWL-1, 500),
@@ -154,19 +175,23 @@ try:
     #    vis.updateLongSection(LongSecFig, ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
 except:
     logging.exception('Unsteady solution for test 3 failed after %i timesteps' % OutputTs3.index[-1]) 
-    
+
+# Reporting
 BdyFig = vis.BdyCndFig(OutputTs3)
 BdyFig[0].suptitle('Test 3: Unsteady downstream boundary')
+BdyFig[0].savefig('Test03_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test03_TimeSeries.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs3.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs3['CumVolErr'].iloc[-1], 
               OutputTs3['CumVolErr'].iloc[-1] / (np.sum(OutputTs3['Qin'])*StepSize*HydDt.seconds)))
 #%% Test 4
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 4: Lagoon storage')
+logging.info('<hr>')
+logging.info('<h1>Test 4: Lagoon storage</h1>')
 
 # Setup
-LagArea[40] = 10000
+LagArea[40] = 20000
 ChanDep = SteadyDep.copy()
 ChanVel = SteadyVel.copy()
 
@@ -206,16 +231,20 @@ try:
 except:
     logging.exception('Unsteady solution for test 4 failed after %i timesteps' % OutputTs4.index[-1]) 
 
+# Reporting
 BdyFig = vis.BdyCndFig(OutputTs4)
 BdyFig[0].suptitle('Test 4: Lagoon Storage')
+BdyFig[0].savefig('Test04_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test04_TimeSeries.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs4.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs4['CumVolErr'].iloc[-1], 
               OutputTs4['CumVolErr'].iloc[-1] / (np.sum(OutputTs4['Qin'])*StepSize*HydDt.seconds)))
     
 #%% Test 5
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 5: Reverse flow')
+logging.info('<hr>')
+logging.info('<h1>Test 5: Reverse flow</h1>')
 
 # Setup
 LagArea[40] = 250000
@@ -257,17 +286,21 @@ try:
 except:
     logging.exception('Unsteady solution for test 5 failed after %i timesteps' % OutputTs5.index[-1]) 
 
+# Reporting
 #    vis.updateLongSection(LongSecFig, ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
 BdyFig = vis.BdyCndFig(OutputTs5)
 BdyFig[0].suptitle('Test 5: Reverse flow')
+BdyFig[0].savefig('Test05_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test05_TimeSeries.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs5.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs5['CumVolErr'].iloc[-1], 
               OutputTs5['CumVolErr'].iloc[-1] / (np.sum(OutputTs5['Qin'])*StepSize*HydDt.seconds)))
 
 #%% Test 6
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 6: Flow profile through constriction')
+logging.info('<hr>')
+logging.info('<h1>Test 6: Flow profile through constriction</h1>')
 
 # Setup
 ChanWidth[-30:-26] = 55
@@ -289,7 +322,7 @@ OutputTs6 = pd.DataFrame(list(zip([Qin],[Qin],[DsWL],[VolumeInModel],[VolErr],[V
 try:
     # Unsteady solution with lagoon storage
     LongSecFig = vis.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
-    LongSecFig[0].suptitle('Test 6: Flow profile through constriction - unsteady solution')
+    LongSecFig[0].suptitle('Test 6: Flow profile through constriction')
     StepSize = 5 
     for ii in range(0, DsWl_Ts.size, StepSize):
         TimesToRun = np.arange(ii, min(ii+StepSize, DsWl_Ts.size))
@@ -316,23 +349,29 @@ except:
 # Reporting
 BdyFig = vis.BdyCndFig(OutputTs6)
 BdyFig[0].suptitle('Test 6: Flow profile through constriction')
+BdyFig[0].savefig('Test06_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test06_TimeSeries.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs6.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs6['CumVolErr'].iloc[-1], 
               OutputTs6['CumVolErr'].iloc[-1] / (np.sum(OutputTs6['Qin'])*StepSize*HydDt.seconds)))
 
 (SteadyDep6, SteadyVel6) = riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
-                                       Roughness, NumericalPars['Beta'], Qin, DsWL)
+                                           Roughness, BetaSteady, Qin, DsWL)
 DepErr = ChanDep - SteadyDep6
 VelErr = ChanVel - SteadyVel6
 LongSecFig[1].plot(Dist, ChanElev + SteadyDep6, 'm--', label='Steady WL')
 LongSecFig[4].plot(Dist, SteadyVel6, 'm--', label='Steady vel')
 LongSecFig[5].plot(Dist, (abs(SteadyVel6)/np.sqrt(9.81*SteadyDep6)), 'm--', label='Steady Fr')
+LongSecFig[0].savefig('Test06_LongSection.png')
+plt.close(LongSecFig[0])
+logging.info('<img src="Test06_LongSection.png">')
 logging.info('Maximum difference between steady and unsteady solutions is: Depth %f m and velocity %f m/s' % (np.max(np.abs(DepErr)), np.max(np.abs(VelErr))))
 
 #%% Test 7
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 7: trans-critical flow')
+logging.info('<hr>')
+logging.info('<h1>Test 7: trans-critical flow</h1>')
 
 # Setup
 ChanWidth[-11:-9] = 25
@@ -354,7 +393,7 @@ OutputTs7 = pd.DataFrame(list(zip([Qin],[Qin],[DsWL],[VolumeInModel],[VolErr],[V
 try:
     # Unsteady solution with lagoon storage
     LongSecFig = vis.longSection(ChanDx, ChanElev, ChanWidth, ChanDep, ChanVel)
-    LongSecFig[0].suptitle('Test 7: Trans-critical flow through constriction - unsteady solution')
+    LongSecFig[0].suptitle('Test 7: Trans-critical flow through constriction')
     StepSize = 5 
     for ii in range(0, DsWl_Ts.size, StepSize):
         TimesToRun = np.arange(ii, min(ii+StepSize, DsWl_Ts.size))
@@ -381,23 +420,29 @@ except:
 # Reporting
 BdyFig = vis.BdyCndFig(OutputTs7)
 BdyFig[0].suptitle('Test 7: Trans-critical flow through constriction')
+BdyFig[0].savefig('Test07_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test07_TimeSeries.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs7.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs7['CumVolErr'].iloc[-1], 
               OutputTs7['CumVolErr'].iloc[-1] / (np.sum(OutputTs7['Qin'])*StepSize*HydDt.seconds)))
 
 (SteadyDep7, SteadyVel7) = riv.solveSteady(ChanDx, ChanElev, ChanWidth, 
-                                       Roughness, NumericalPars['Beta'], Qin, DsWL)
+                                           Roughness, BetaSteady, Qin, DsWL)
 DepErr = ChanDep - SteadyDep7
 VelErr = ChanVel - SteadyVel7
 LongSecFig[1].plot(Dist, ChanElev + SteadyDep7, 'm--', label='Steady WL')
 LongSecFig[4].plot(Dist, SteadyVel7, 'm--', label='Steady vel')
 LongSecFig[5].plot(Dist, abs(SteadyVel7)/np.sqrt(9.81*SteadyDep7), 'm--', label='Steady Fr')
+LongSecFig[0].savefig('Test07_LongSection.png')
+plt.close(LongSecFig[0])
+logging.info('<img src="Test07_LongSection.png">')
 logging.info('Maximum difference between steady and unsteady solutions is: Depth %f m and velocity %f m/s' % (np.max(np.abs(DepErr)), np.max(np.abs(VelErr))))
 
 #%% Test 8
-logging.info('-----------------------------------------------------------------------')
-logging.info('Test 8: Dynamic supercritical flow')
+logging.info('<hr>')
+logging.info('<h1>Test 8: Dynamic supercritical flow</h1>')
 
 # Setup
 ChanWidth[-11:-9] = 20
@@ -447,6 +492,12 @@ except:
 # Reporting
 BdyFig = vis.BdyCndFig(OutputTs8)
 BdyFig[0].suptitle('Test 8: Dynamic supercritical flow')
+BdyFig[0].savefig('Test08_TimeSeries.png')
+plt.close(BdyFig[0])
+logging.info('<img src="Test08_TimeSeries.png">')
+LongSecFig[0].savefig('Test08_LongSection.png')
+plt.close(LongSecFig[0])
+logging.info('<img src="Test08_LongSection.png">')
 logging.info('Maximum mass balance error = %f%% (of inflow)' % np.max(np.abs(OutputTs8.VolErrPerc)))
 logging.info('Cumulative volumetric error over simulation = %f m3 (%f%% of total inflow)' % 
              (OutputTs8['CumVolErr'].iloc[-1], 
