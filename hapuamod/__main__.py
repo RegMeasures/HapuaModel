5b34a70da2a6d67a8db473f5860fbad2dbd503db
RegMeasures <Richard.measures@niwa.co.nz>
2019-10-25 09:36:44 +1300

    Minor improvements to model description in argparse Description given on commandline help improved

--- hapuamod/__main__.py
+++ hapuamod/__main__.py
@@ -3,10 +3,11 @@ import argparse
 from .core import main
 
 # Parse arguments from commandline
-Parser = argparse.ArgumentParser(description='Hapua model')
+Parser = argparse.ArgumentParser(description='HapuaMod is a model which simulates morphological evolution of hapua type rivermouth lagoons. ' +
+                                             'For a full description of the model and how to use it see https://github.com/RegMeasures/HapuaModel/wiki')
 Parser.add_argument('ModelConfigFile', type=str, help='Model config file (*.cnf)')
 Parser.add_argument('-o', '--Overwrite', action='store_true', help='Optional flag to overwrite output file without checking')
 ArgsIn = Parser.parse_args()
-# TODO: Add verbosity and overwrite commandline arguments
+# TODO: Add verbosity commandline argument
 
 main(ArgsIn.ModelConfigFile, ArgsIn.Overwrite)
