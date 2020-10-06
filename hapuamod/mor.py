5edc0474c994fd6adf914e58fb525255e3825e4c
RegMeasures <Richard.measures@niwa.co.nz>
2020-10-07 11:39:20 +1300

    Preventing migration of outlet beyond edge of domain Addresses #77 by preventing migration beyond the transects 1-in from the ends of the domain.

--- hapuamod/mor.py
+++ hapuamod/mor.py
@@ -210,6 +210,14 @@ def updateMorphology(ShoreX, ShoreY, ShoreZ,
         OutletEndElev.flat[0] += OutletEndAggRate * MorDt.seconds
         OutletEndX[1] += OutletEndXMoveRate * MorDt.seconds
     
+    #%% Check if outlet has extended too close to the edge of the domain
+    if OutletEndX[1] < ShoreX[1]:
+        logging.info('Left (negative X) end of outlet channel too close to edge of domain - preventing further migration')
+        OutletEndX[1] = ShoreX[1]
+    if OutletEndX[1] > ShoreX[-2]:
+        logging.info('Right (negative X) end of outlet channel too close to edge of domain - preventing further migration')
+        OutletEndX[1] = ShoreX[1]
+    
     #%% Check if shoreline has eroded into cliff anywhere
     CliffCollision = ShoreY[:,0] <= ShoreY[:,4]
     if np.any(CliffCollision):
